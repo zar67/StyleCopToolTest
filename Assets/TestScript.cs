747a78367d4b8e945cc46af86aada67f3e34e979
ZRowbotham <ZRowbotham@sumo-digital.com>
2021-03-10 11:48:20 +0000

    Test

---
+++ Assets/TestScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Using StyleCop.Analysers NuGet package
+ * Set the code standard in Tools->Options->TextEditor->C#->CodeStyle
+ * Generate the .editorconfig settings to update the file
+ * Naming and formating can be done just from the Visual Studio settings (no StyleCop needed)
+ * Style cop adds documentation requirements (XML summaries etc) as well as some formatting
+ * (see stylecop.json for full configuration settings and References/Analysers/StyleCop.Analysers for the analysers)
+*/
+
+public class TestScript : MonoBehaviour
+{
+    private int test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
